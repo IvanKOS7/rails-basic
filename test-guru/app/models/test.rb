5fa9b86ad379a2bb434bf0748f0322ae0e1e54ab
IvanKOS7 <ivankos429@gmail.com>
2022-01-20 18:56:33 +0700

    Correction #4

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -3,9 +3,9 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :questions
 
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: {scope: :level}
   validates :level, numericality: { greater_than_or_equal_to: 0 }
-  validates :title, :level, uniqueness: true
+  
 #\A[+]?\d+\z - regexp
   def self.tests_categories_sort(category_name)
     all_test_with_category.order(title: :desc).where("categories.title like ? ", category_name).pluck(:title)
