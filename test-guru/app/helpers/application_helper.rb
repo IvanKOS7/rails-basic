da78a6019f9743f58d6fe9d943cef2a3656e189c
IvanKOS7 <89787153+IvanKOS7@users.noreply.github.com>
2022-02-09 19:12:32 +0700

    Revert "Add lesson-8"

--- test-guru/app/helpers/application_helper.rb
+++ test-guru/app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  
-  def current_year
-    year = Time.new.year
-  end
-#IvanKOS7
-#ruby-basics
-  def github_url(author, repo)
-    link_to 'GITHUB', "https://github.com/#{author}/#{repo}", target: "_blank"
-  end
 end
--- test-guru/app/helpers/questions_helper.rb
+++ test-guru/app/helpers/questions_helper.rb
@@ -1,10 +1,2 @@
 module QuestionsHelper
-
-  def question_header(question)
-    if question.new_record?
-      "Create New question for test: #{question.test.title}"
-    else
-      "Edit question. Test: #{question.test.title} <h1>"
-    end
-  end
 end
