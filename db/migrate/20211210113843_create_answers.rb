f7690d074e6b13f4fdb53ccad69a2a47cbbd9377
IvanKOS7 <ivankos429@gmail.com>
2022-02-13 14:29:22 +0700

    Delete cache

--- db/migrate/20211210113843_create_answers.rb
+++ db/migrate/20211210113843_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
       t.boolean :correct
-      t.string :body
+      t.string :answer
       t.belongs_to :question, foreign_key: true
 
       t.timestamps
