0ae4201dd3f4f190c8905438719416219dacae03
IvanKOS7 <ivankos429@gmail.com>
2022-03-12 20:31:16 +0700

    Correction

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :user, class_name: "User"
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :test_passages, dependent: :destroy
 
   has_many :users, through: :test_passages
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_081601) do
+ActiveRecord::Schema.define(version: 2022_03_01_161009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
