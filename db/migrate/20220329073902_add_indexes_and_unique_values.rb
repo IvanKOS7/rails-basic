959655b617c7a48a0ca5828c70ddf05ad212a1d9
IvanKOS7 <ivankos429@gmail.com>
2022-03-29 18:50:50 +0700

    correction db

--- db/migrate/20220329073902_add_indexes_and_unique_values.rb
+++ db/migrate/20220329073902_add_indexes_and_unique_values.rb
@@ -1,13 +1,11 @@
 class AddIndexesAndUniqueValues < ActiveRecord::Migration[6.1]
   def up
     add_index :tests, %i[title level], unique: true
-    add_column :categories, :title, :string
     add_index :categories, :title, unique: true
   end
 
   def down
     remove_index :categories, :title, unique: true
     remove_index :tests, %i[title level], unique: true
-    remove_column :categories, :title, :string
   end
 end
