8b992dc160bbd36a2bcb821a8f89bbd1e1aa4ef3
IvanKOS7 <ivankos429@gmail.com>
2022-03-31 17:11:51 +0700

    views:test-passage:result#1

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,7 +1,7 @@
 module TestPassagesHelper
 
   def result_handler(resource)
-    resource.test_passed? ? content_tag(:h2, "Good", class: "green") : content_tag(:h2, "Test Failed(", class: "red")
+    resource.test_passed? ? content_tag(:h2, "Good", class: "text-success") : content_tag(:h2, "Test Failed(", class: "text-danger")
   end
 
 end
