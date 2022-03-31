2eaad6f2a7b760dbed4c2efd8c6ef45aa1304ccd
IvanKOS7 <ivankos429@gmail.com>
2022-03-31 15:19:14 +0700

    action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_otions = { host: 'radiant-everglades-76824.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:  'smtp.yandex.ru'
+    address:  'smtp.yandex.ru',
     port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
