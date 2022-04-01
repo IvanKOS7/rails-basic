4fa6a1ea62a061057b7ba3c040e503b58de84c30
IvanKOS7 <ivankos429@gmail.com>
2022-04-01 17:17:57 +0700

    correction#5tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -11,7 +11,7 @@ class TestsController < ApplicationController
   def start
     if @test.questions.empty?
       flash.now[:alert] = "trhrhthrt"
-      render :index
+      redirect_to root_path
     else
       current_user.tests.push(@test)
       redirect_to current_user.test_passage(@test)
