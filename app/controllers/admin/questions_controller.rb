dd5874d78fd93160a0885b2b589126bac85c668f
IvanKOS7 <ivankos429@gmail.com>
2022-04-04 21:42:53 +0700

    correction#navigation

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -18,7 +18,7 @@ class Admin::QuestionsController < Admin::BaseController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to admin_tests_path
+      redirect_to admin_test_path(@test), notice: "Question successfuly created"
     else
       render :new
     end
@@ -29,7 +29,7 @@ class Admin::QuestionsController < Admin::BaseController
 
   def update
     if @question.update(question_params)
-      redirect_to [:admin, @question]
+      redirect_to admin_test_path(@question.test), notice: "Question successfuly updated"
     else
       render :edit
     end
